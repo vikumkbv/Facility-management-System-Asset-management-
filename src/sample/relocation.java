73a705947032265c2f2558915e279e2be6d5d823
vikumkbv <vikumkbv@esportlk.com>
2018-08-06 21:12:03 +0530

    asset relocation

---
+++ src/sample/relocation.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class relocation implements Initializable {
+
+   /** @FXML
+    private TableView<Asset> table;
+    @FXML private TableColumn<Asset, String> buildingId;
+    @FXML private TableColumn<Asset, Integer> floorId;
+    @FXML private TableColumn<Asset, String> roomId;
+    @FXML private TableColumn<Asset, String> itemName;
+    @FXML private TableColumn<Asset, String> itemType;
+    @FXML private TableColumn<Asset, String> purchaseCost;
+    @FXML private TableColumn<Asset, String> purchaseDate;
+    @FXML private TableColumn<Asset, String> supplier;
+    @FXML private TableColumn<Asset , String> barCode;
+    @FXML private TableColumn<Asset, String> keyNumber;
+    @FXML private TableColumn<Asset, Boolean> markedRepair;
+    @FXML private TableColumn<Asset, Boolean> markedSold;**/
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
